4a59dbd761dc514f1a3a9794602fb0d3db65663f
Hunter Jackson <hjackson@sightmachine.com>
2021-03-20 15:23:11 -0700

    create a heap

--- src/main/java/sorting/Heap.java
+++ src/main/java/sorting/Heap.java
@@ -1,5 +1,118 @@
 package sorting;
 
-public class Heap<T> {
+import java.lang.reflect.Array;
+import java.util.Collection;
+import java.util.OptionalInt;
 
+/**
+ * Limited implementation of a heap data structure
+ *
+ * <p>Known limitation are that the max ever size of the heap is the size of the initializing
+ * collection, and the heap cannot be initialized with an empty collection.
+ *
+ * @param <T> element type
+ */
+public final class Heap<T extends Comparable<? super T>> {
+
+  private final boolean minHeap;
+
+  // zeroth element is always ignored for easy position calculation
+  private final T[] heap;
+  private int end = 0;
+
+  public static <T extends Comparable<? super T>> Heap<T> minHeap(Collection<T> elements) {
+    return new Heap<>(true, elements);
+  }
+
+  public static <T extends Comparable<? super T>> Heap<T> maxHeap(Collection<T> elements) {
+    return new Heap<>(false, elements);
+  }
+
+  private Heap(boolean minHeap, Collection<T> elements) {
+    assert elements.size() > 0;
+    this.minHeap = minHeap;
+    @SuppressWarnings("unchecked")
+    T[] heap = (T[]) Array.newInstance(elements.iterator().next().getClass(), elements.size() + 1);
+    this.heap = heap;
+    elements.forEach(this::insert);
+  }
+
+  public Heap<T> insert(T element) {
+    assert end < heap.length;
+    end += 1;
+    heap[end] = element;
+    bubbleUp(end);
+    return this;
+  }
+
+  private void bubbleUp(int position) {
+    int parentPosition = position / 2;
+    if (parentPosition <= 0) {
+      return;
+    }
+
+    T element = heap[position];
+    T parent = heap[parentPosition];
+
+    int comparison = minHeap ? element.compareTo(parent) : -element.compareTo(parent);
+    if (comparison < 0) {
+      heap[position] = parent;
+      heap[parentPosition] = element;
+      bubbleUp(parentPosition);
+    }
+  }
+
+  public T head() {
+    return heap[1];
+  }
+
+  public T pop() {
+    T out = heap[1];
+    heap[1] = heap[end];
+    end -= 1;
+    bubbleDown(1);
+    return out;
+  }
+
+  private void bubbleDown(int position) {
+    OptionalInt dominantChildPosition = dominantChildPosition(position);
+    if (dominantChildPosition.isEmpty()) {
+      return;
+    }
+    int childPosition = dominantChildPosition.getAsInt();
+    T element = heap[position];
+    int comparison =
+        minHeap ? element.compareTo(heap[childPosition]) : -element.compareTo(heap[childPosition]);
+    if (comparison > 0) {
+      heap[position] = heap[childPosition];
+      heap[childPosition] = element;
+      bubbleDown(childPosition);
+    }
+  }
+
+  private OptionalInt dominantChildPosition(int position) {
+    int leftChildPosition = position * 2;
+    if (leftChildPosition > end) {
+      return OptionalInt.empty();
+    }
+
+    int rightChildPosition = leftChildPosition + 1;
+    if (rightChildPosition > end) {
+      return OptionalInt.of(leftChildPosition);
+    }
+
+    int comparison = heap[leftChildPosition].compareTo(heap[rightChildPosition]);
+    if (minHeap) {
+      comparison *= -1;
+    }
+    if (comparison > 0) {
+      return OptionalInt.of(leftChildPosition);
+    } else {
+      return OptionalInt.of(rightChildPosition);
+    }
+  }
+
+  public int size() {
+    return end;
+  }
 }
---
+++ src/test/java/sorting/HeapTest.java
@@ -0,0 +1,66 @@
+package sorting;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class HeapTest {
+
+  private static final List<Integer> descendingList = List.of(9, 8, 7, 6, 5, 4, 3, 2, 1, 0);
+  private static final List<Integer> ascendingList = List.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+  private static final List<Integer> arbitraryList =
+      List.of(3, 2, 1, 2, 2, 4, 5, 6, 3, 8, 8, 6, 7, 4, 6, 9);
+
+  private static Stream<Collection<Integer>> collectionProvider() {
+    return Stream.of(descendingList, ascendingList, arbitraryList);
+  }
+
+  @ParameterizedTest
+  @MethodSource("collectionProvider")
+  void getMin(Collection<Integer> elements) {
+    Heap<Integer> heap = Heap.minHeap(elements);
+    assertEquals(heap.head(), Collections.min(elements));
+  }
+
+  @ParameterizedTest
+  @MethodSource("collectionProvider")
+  void getMax(Collection<Integer> elements) {
+    Heap<Integer> heap = Heap.maxHeap(elements);
+    assertEquals(heap.head(), Collections.max(elements));
+  }
+
+  @ParameterizedTest
+  @MethodSource("collectionProvider")
+  void popMin(List<Integer> elements) {
+    Heap<Integer> heap = Heap.minHeap(elements);
+    List<Integer> fromHeap = new ArrayList<>(elements.size());
+    while (heap.size() > 0) {
+      fromHeap.add(heap.pop());
+    }
+
+    List<Integer> sortedElements = elements.stream().sorted().collect(Collectors.toList());
+    assertEquals(fromHeap, sortedElements);
+  }
+
+  @ParameterizedTest
+  @MethodSource("collectionProvider")
+  void popMax(List<Integer> elements) {
+    Heap<Integer> heap = Heap.maxHeap(elements);
+    List<Integer> fromHeap = new ArrayList<>(elements.size());
+    while (heap.size() > 0) {
+      fromHeap.add(heap.pop());
+    }
+
+    List<Integer> sortedElements =
+        elements.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+    assertEquals(fromHeap, sortedElements);
+  }
+}
